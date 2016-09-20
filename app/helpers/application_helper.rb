9cfff175c04698510ef49c8dabe234cc73702554
Khoi Nguyen <khnaero@gmail.com>
2016-09-20 12:09:39 -0500

    Change base title to AFT

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
-		base_title = "Austin Parcourses"
+		base_title = "AFT"
 		if page_title.empty?
 			base_title
 		else
